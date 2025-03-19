7853420b5adb83b214b585fcac88b018b45d8f6f
Chris Yager <43963367+dcyager359@users.noreply.github.com>
2025-03-19 09:00:32 -0400

    put root url in configuration

--- src/main/java/com/cyager/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/com/cyager/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -1,5 +1,6 @@
 package com.cyager.spring6resttemplate.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,14 +10,17 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 @Configuration
 public class RestTemplateBuilderConfig {
 
+    @Value("${rest.template.rootUrl}")
+    String rootUrl;
+
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
+
+        assert (rootUrl != null);
+
         RestTemplateBuilder restTemplateBuilder = configurer.configure(new RestTemplateBuilder());
         DefaultUriBuilderFactory uriBuilderFactory =
-                new DefaultUriBuilderFactory("http://localhost:8080");
+                new DefaultUriBuilderFactory(rootUrl);
         return restTemplateBuilder.uriTemplateHandler(uriBuilderFactory);
     }
-
-//    public final String HOST_NAME = "localhost:8080";
-//    public final String URL_PATH = "/api/v1/beer";
 }
